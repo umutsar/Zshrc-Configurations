955a2fb9862b58da30ace20e23ef8b3cded15fe5
Umut Şar <contact@umutsar.net>
2024-03-03 20:50:28 +0300

    Create ChangeDirectoryWithoutParemeter.sh

---
+++ zsh/ChangeDirectoryWithoutParemeter.sh
@@ -0,0 +1,9 @@
+alias shortcutFolderName='cd /Path/To/Your'
+
+
+# Implementation:
+
+# nano ~/.zshrc
+# go to the bottom of the file and write above codes. 
+# ctrl + o and ctrl + x (for save and exit)
+# source ~/.zshrc ( For persistent implementation.)
