6b2ba7bce40b11cd9422ee57519ee85525417b45
Umut Şar <contact@umutsar.net>
2024-09-12 04:17:41 +0300

    Create TerminalPrompt.zsh

---
+++ zsh/TerminalPrompt.zsh
@@ -0,0 +1,8 @@
+PROMPT='%B%F{blue}-> %n:[%~]# %f%b'
+
+# %B means bold text. It must be used with < %b >
+# %F means color of text. It must used with {color_name} and finish tag < %f >
+# %n means your domain. Such as 'YourName'
+# %~ means path to current terminal.
+
+#  ], [, ->, # symbols are only string. They are not specific code.
